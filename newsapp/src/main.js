7f2a27d62640b7aced14ceb7c2492ec758c47703
Vivek <vivekjeevakumar1110@gmail.com>
2020-07-23 11:27:51 +0700

    Basic setup of vuex, vuetify and vue resource done

--- newsapp/src/main.js
+++ newsapp/src/main.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+const router = new VueRouter({
+  mode: 'history'
+})
 
 new Vue({
+  vuetify,
+  router,
   el: '#app',
   render: h => h(App)
 })
