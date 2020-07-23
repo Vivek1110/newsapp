97f16de03f231b7f42572c1d7ab05d891120dcf2
Vivek <vivekjeevakumar1110@gmail.com>
2020-07-23 11:41:17 +0700

    Added header to app

--- newsapp/src/main.js
+++ newsapp/src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import VueRouter from 'vue-router';
 import VueResource from 'vue-resource';
+import { routes } from './route'
 
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
 const router = new VueRouter({
+  routes,
   mode: 'history'
 })
 
